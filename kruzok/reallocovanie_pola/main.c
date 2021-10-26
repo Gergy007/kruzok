0d28c65d9c84d589764d4c3d4cc214970e7831ca
Gergy007 <90184932+Gergy007@users.noreply.github.com>
2021-10-26 17:00:13 +0200

    Add files via upload

---
+++ kruzok/reallocovanie_pola/main.c
@@ -0,0 +1,42 @@
+//skusit spravit nekonecne pole intov - po konci poctu sa opyta ci chce este nacitavat, ak ano tak reallocuje pamat a nacita dalej
+
+#include <stdio.h>
+#include <stdlib.h>
+#define POCET 10
+
+int main()
+{
+    int znak;
+    size_t pocetZnakov = 0;
+    size_t pocetRealokacii = 1;
+    char *veta;
+    if((veta = (char*)malloc(sizeof(char)*POCET)) == NULL)
+    {
+        puts("Not allocated!");
+        exit(1);
+    }
+
+    while((znak = getc(stdin)) != '\n') //citanie znakov azdo konca riadka = Enter = \n
+    {
+        ++pocetZnakov;
+        if(pocetZnakov % POCET == 0) // uz som na nasobku 10 pri pocte znakov, musim realocovat
+        {
+            ++pocetRealokacii;
+            if((veta = (char *)realloc(veta,POCET*pocetRealokacii*sizeof(char))) == NULL)
+            {
+                puts("Chyba alokacie pamate!");
+                exit(1);
+            }
+        }
+        *(veta+pocetZnakov-1) = znak; //zapisem znak do pola
+    }
+    *(veta+pocetZnakov) = '\0';
+    puts(veta);
+    printf("Zadali sme %ld znakov a %ld-krat sme reallocovali pole",pocetZnakov,pocetRealokacii);
+
+
+    free(veta);
+    veta = NULL;
+
+    return 0;
+}
