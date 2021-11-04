c56b78b49cb75ec2e5b2e554a6858e3115f4ba07
Gergy007 <90184932+Gergy007@users.noreply.github.com>
2021-11-04 17:17:32 +0100

    Add files via upload

--- kruzok/reallocovanie_pola/main.c
+++ kruzok/reallocovanie_pola/main.c
@@ -2,21 +2,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #define POCET 10
 
 int main()
 {
     int znak;
+    bool go = true;
+    int rozhodni;
+    size_t pocetIntov = 0;
     size_t pocetZnakov = 0;
     size_t pocetRealokacii = 1;
     char *veta;
+
     if((veta = (char*)malloc(sizeof(char)*POCET)) == NULL)
     {
         puts("Not allocated!");
         exit(1);
     }
 
-    while((znak = getc(stdin)) != '\n') //citanie znakov azdo konca riadka = Enter = \n
+    while((znak = getc(stdin)) != '\n') //citanie znakov az do konca riadka = Enter = \n
     {
         ++pocetZnakov;
         if(pocetZnakov % POCET == 0) // uz som na nasobku 10 pri pocte znakov, musim realocovat
@@ -32,11 +38,46 @@ int main()
     }
     *(veta+pocetZnakov) = '\0';
     puts(veta);
-    printf("Zadali sme %ld znakov a %ld-krat sme reallocovali pole",pocetZnakov,pocetRealokacii);
+    printf("Zadali sme %ld znakov a %ld-krat sme reallocovali pole.\n",pocetZnakov,pocetRealokacii-1);
 
 
-    free(veta);
-    veta = NULL;
+    pocetRealokacii = 1;
+    int *inty;
+    if((inty = (int *) malloc(sizeof (int)*POCET)) == NULL)
+    {
+        puts("Not allocated!\n");
+        exit(1);
+    }
 
-    return 0;
+    while (go == true)
+    {
+        ++pocetIntov;
+        scanf("%d",inty+pocetIntov-1);
+        if((pocetIntov % POCET) == 0)
+        {
+            puts("Chces este zadavat cisla? (ano - 1; nie - 0)");
+            scanf("%d",&rozhodni);
+            if(rozhodni == 1)
+            {
+                ++pocetRealokacii;
+                if((inty = (int *) realloc(inty,POCET*pocetRealokacii*sizeof (int))) == NULL)
+                {
+                    puts("Chyba alokacie pamate\n");
+                    exit(1);
+                }
+            }
+            else
+            {
+                go = false;
+            }
+        }
+    }
+
+    printf("Zadali sme %ld cisel a pole sme reallocovali %ld-krat.\n",pocetIntov,pocetRealokacii-1);
+
+    free(inty);
+    inty = NULL;
+    //free(veta);
+    //veta = NULL;
+
 }
